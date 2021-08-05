7365563b2baa3b4a3ba6716db3b83c23c3341cb2
Kirill Aksenov <kirill.aksenov@internet.ru>
2021-08-05 15:54:50 +0300

    lesson03 intermediate commit

---
+++ lesson03/app/src/main/java/ru/geekbraines/lesson03/Calculator.java
@@ -0,0 +1,139 @@
+package ru.geekbraines.lesson03;
+
+import java.text.DecimalFormat;
+
+public class Calculator {
+
+    private double firstArg;
+    private double secondArg;
+    private int actionSelected;
+
+    private StringBuilder inputStr = new StringBuilder();
+    private DecimalFormat decimalFormat = new DecimalFormat( "#.###" );
+    private String result;
+
+    private State state;
+
+    private enum State {
+        firstArgInput,
+        secondArgInput,
+        resultShow
+    }
+
+    public Calculator() {
+        state = State.firstArgInput;
+    }
+
+    public void onNumPressed(int buttonId) {
+
+
+        if (state == State.resultShow) {
+            state = State.firstArgInput;
+            inputStr.setLength(0);
+            result = "";
+        }
+
+        if (inputStr.length() < 9 || state == State.secondArgInput && inputStr.length() < 21) {
+            switch (buttonId) {
+                case R.id.num_0:
+                    if (inputStr.length() != 0) {
+                        inputStr.append("0");
+                    }
+                    break;
+                case R.id.num_1:
+                    inputStr.append("1");
+                    break;
+                case R.id.num_2:
+                    inputStr.append("2");
+                    break;
+                case R.id.num_3:
+                    inputStr.append("3");
+                    break;
+                case R.id.num_4:
+                    inputStr.append("4");
+                    break;
+                case R.id.num_5:
+                    inputStr.append("5");
+                    break;
+                case R.id.num_6:
+                    inputStr.append("6");
+                    break;
+                case R.id.num_7:
+                    inputStr.append("7");
+                    break;
+                case R.id.num_8:
+                    inputStr.append("8");
+                    break;
+                case R.id.num_9:
+                    inputStr.append("9");
+                    break;
+                case R.id.point:
+                    inputStr.append(".");
+                    break;
+            }
+        }
+
+    }
+
+    public void onActionPressed(int actionId) {
+
+        if (actionId == R.id.clear) {
+            inputStr.setLength(0);
+            result = "";
+        }
+
+        if (actionId == R.id.equals && state == State.secondArgInput) {
+            int position = inputStr.indexOf(" ");
+            String secondArg = inputStr.substring(position + 3);
+            this.secondArg = Double.parseDouble(secondArg);
+            state = State.resultShow;
+            inputStr.setLength(0);
+
+            switch (actionSelected) {
+                case R.id.plus:
+                    result = decimalFormat.format(firstArg + this.secondArg);
+                    break;
+                case R.id.minus:
+                    result = decimalFormat.format(firstArg - this.secondArg);
+                    break;
+                case R.id.multiply:
+                    result = decimalFormat.format(firstArg * this.secondArg);
+                    break;
+                case R.id.divide:
+                    result = decimalFormat.format(firstArg / this.secondArg);
+                    break;
+            }
+
+        } else if (inputStr.length() > 0 && state == State.firstArgInput) {
+            firstArg = Double.parseDouble(inputStr.toString());
+            state = State.secondArgInput;
+
+            switch (actionId) {
+                case R.id.plus:
+                    actionSelected = R.id.plus;
+                    inputStr.append(" + ");
+                    break;
+                case R.id.minus:
+                    actionSelected = R.id.minus;
+                    inputStr.append(" - ");
+                    break;
+                case R.id.multiply:
+                    actionSelected = R.id.multiply;
+                    inputStr.append(" × ");
+                    break;
+                case R.id.divide:
+                    actionSelected = R.id.divide;
+                    inputStr.append(" ÷ ");
+                    break;
+            }
+        }
+    }
+
+    public String getActions() {
+        return inputStr.toString();
+    }
+
+    public String getResult() {
+        return result;
+    }
+}
--- lesson03/app/src/main/java/ru/geekbraines/lesson03/MainActivity.java
+++ lesson03/app/src/main/java/ru/geekbraines/lesson03/MainActivity.java
@@ -3,12 +3,75 @@ package ru.geekbraines.lesson03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Calculator calculator;
+
+    private TextView tvActions;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int[] numbersIds = new int[]{
+                R.id.num_0,
+                R.id.num_1,
+                R.id.num_2,
+                R.id.num_3,
+                R.id.num_4,
+                R.id.num_5,
+                R.id.num_6,
+                R.id.num_7,
+                R.id.num_8,
+                R.id.num_9,
+                R.id.point
+        };
+
+        int[] actionsIds = new int[]{
+                R.id.plus,
+                R.id.minus,
+                R.id.divide,
+                R.id.multiply,
+                R.id.equals,
+                R.id.clear
+        };
+
+        tvActions = findViewById(R.id.tv_actions);
+        tvResult = findViewById(R.id.tv_result);
+
+        calculator = new Calculator();
+
+        View.OnClickListener numberButtonOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.onNumPressed(v.getId());
+                tvActions.setText(calculator.getActions());
+                tvResult.setText(calculator.getResult());
+            }
+        };
+
+        View.OnClickListener actionButtonOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.onActionPressed(v.getId());
+                if (v.getId() != R.id.equals) {
+                    tvActions.setText(calculator.getActions());
+                }
+                tvResult.setText(calculator.getResult());
+            }
+        };
+
+        for (int i = 0; i < numbersIds.length; i++) {
+            findViewById(numbersIds[i]).setOnClickListener(numberButtonOnClickListener);
+        }
+
+        for (int i = 0; i < actionsIds.length; i++) {
+            findViewById(actionsIds[i]).setOnClickListener(actionButtonOnClickListener);
+        }
     }
 }
