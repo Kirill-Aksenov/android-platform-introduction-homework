ecaa253b57ac4fb1e253e03846b3e0cba0c05404
Kirill Aksenov <kirill.aksenov@internet.ru>
2021-08-21 12:39:11 +0300

    lesson 05 intermediate commit

--- lesson05/app/src/main/java/ru/geekbraines/lesson05/SettingsActivity.java
+++ lesson05/app/src/main/java/ru/geekbraines/lesson05/SettingsActivity.java
@@ -3,6 +3,7 @@ package ru.geekbraines.lesson05;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,33 +11,65 @@ import android.widget.RadioButton;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private static final String KEY_THEME = "keyTheme";
+
+    private SharedPreferences sharedPreferences;
+
+    private RadioButton radioButtonLight;
+    private RadioButton radioButtonDark;
+    private Button buttonReturn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        RadioButton radioButtonLight = findViewById(R.id.radioButtonLight);
+        sharedPreferences = getSharedPreferences(KEY_THEME, MODE_PRIVATE);
+
+        radioButtonLight = findViewById(R.id.radioButtonLight);
+        radioButtonDark = findViewById(R.id.radioButtonDark);
+        buttonReturn = findViewById(R.id.button_return);
+
+        checkTheme();
+
         radioButtonLight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                saveThemeState(false);
             }
         });
 
-        RadioButton radioButtonDark = findViewById(R.id.radioButtonDark);
         radioButtonDark.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                saveThemeState(true);
             }
         });
 
-        Button buttonReturn = findViewById(R.id.button_return);
         buttonReturn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+    }
+
+    // сохраняем выбранную тему через SharedPreferences при помощи булева значения isDarkTheme
+    private void saveThemeState(boolean isDarkTheme) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(KEY_THEME, isDarkTheme).apply();
+    }
+
+    //получаем сохраненные настройки
+    public void checkTheme() {
+        if (sharedPreferences.getBoolean(KEY_THEME, false)) {
+            radioButtonDark.setChecked(true);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            radioButtonLight.setChecked(true);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
     }
 }
